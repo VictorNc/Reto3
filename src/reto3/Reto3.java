b5bd2a327b89e1622a87be861e48e0531fb89c5b
VictorNc <al02775806@tecmilenio.mx>
2019-02-05 09:04:15 -0600

    Acomodo

--- src/reto3/Reto3.java
+++ src/reto3/Reto3.java
@@ -21,12 +21,48 @@ public class Reto3 {
     public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
     
     public static void main(String[] args) throws IOException {
+        int puntaje;
+        int [] valores = new int[3];
+        int temp = 0;
+        int mayor;
         Constructor personaje1 = new Constructor(); 
         Mago personaje2 = new Mago();
         Guerrero personaje3 = new Guerrero();
         System.out.println("Indique puntaje para Constructor ");
-        int puntaje = Integer.parseInt(entrada.readLine());
+        puntaje = Integer.parseInt(entrada.readLine());
         personaje1.setPuntaje(puntaje);
+        System.out.println("Indique puntaje para Mago ");
+        puntaje = Integer.parseInt(entrada.readLine());
+        personaje2.setPuntaje(puntaje);
+        System.out.println("Indique puntaje para Guerrero ");
+        puntaje = Integer.parseInt(entrada.readLine());
+        personaje3.setPuntaje(puntaje);
+        
+        valores[0] = personaje1.getPuntaje();
+        valores[1] = personaje2.getPuntaje();
+        valores[2] = personaje3.getPuntaje();
+        
+        
+        for(int i = 0; i < valores.length; i++)
+        {
+            for(int j = 0; j < valores.length - 1; j-- )
+            {
+                if(valores[j] > valores[j]);
+                {
+                int tmp = valores[j+1]; 
+                valores[j+1] = valores[j];
+                valores[ j ] = temp;
+                }
+            }
+        }
+        for(int y = 0; y < valores.length; y++){
+            //System.out.println(valores[y]);
+            if(personaje1.getPuntaje() == valores [y]){
+                System.out.println("");
+            }
+            
+        }
+        mayor = valores[3];
     }
     
 }
