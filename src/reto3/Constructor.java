3c2ffc968dc09215cded9e6d9f16807a2f5a79f5
VictorNc <al02775806@tecmilenio.mx>
2019-02-05 07:59:15 -0600

    Creación de clases

---
+++ src/reto3/Constructor.java
@@ -0,0 +1,30 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto3;
+
+/**
+ *
+ * @author AlumnoUTM
+ */
+public class Constructor {
+     private int puntaje; 
+
+    public Constructor(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public Constructor() {
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+     
+}
